09f8cefd62fe9b3707e73d3bab6d51522b3e3388
tonylim89 <tonylim9@hotmail.com>
2023-06-15 12:41:45 +0800

    Initial empty springboot upload

---
+++ src/main/java/sg/edu/nus/iss/demo_springboot_sessions/DemoSpringbootSessionsApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.demo_springboot_sessions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootSessionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootSessionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/demo_springboot_sessions/DemoSpringbootSessionsApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.demo_springboot_sessions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootSessionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
